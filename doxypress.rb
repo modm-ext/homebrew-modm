faced10423dded443b4882127f0fc4a26fda326d
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2021-03-26 19:05:55 +0100

    Update to Doxypress 1.4.1

--- doxypress.rb
+++ doxypress.rb
@@ -4,10 +4,10 @@ class Doxypress < Formula
     desc 'DoxyPress: C++ documentation generator'
     homepage 'https://www.copperspice.com/documentation-doxypress.html'
 
-    url 'https://download.copperspice.com/doxypress/binary/doxypress-1.4.0-osx10.14-x64.tar.bz2'
+    url 'https://download.copperspice.com/doxypress/binary/doxypress-1.4.1-osx10.14-x64.tar.bz2'
 
-    sha256 '63a5e21f0d9649fc264dd45d538fb206e0b8d838460dcbc2cbbf73382fe8d9b7'
-    version '1.4.0'
+    sha256 'd50bf9c65c5dd6c02b58a4fe4ecc6ad570404bb45ab55f4e71ef4d42b7cd7b2f'
+    version '1.4.1'
 
     def install
         bin.install "doxypress"
