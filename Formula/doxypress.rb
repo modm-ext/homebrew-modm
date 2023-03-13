2fdad639e78c178e69904a7fd67b0a1bb96f89f9
Raphael Lehmann <raphael@rleh.de>
2023-03-13 23:55:23 +0100

    Move doxypress to Formula/ and update to 1.5.1

--- Formula/doxypress.rb
+++ Formula/doxypress.rb
@@ -4,10 +4,10 @@ class Doxypress < Formula
     desc 'DoxyPress: C++ documentation generator'
     homepage 'https://www.copperspice.com/documentation-doxypress.html'
 
-    url 'https://github.com/copperspice/doxypress/releases/download/dp-1.4.2/doxypress-1.4.2-osx10.16-x64.tar.bz2'
+    url 'https://github.com/copperspice/doxypress/releases/download/dp-1.5.1/doxypress-1.5.1-osx11.5-x64.tar.bz2'
 
-    sha256 '13eaec9e313f8212c6c8f8b61fabfc225c960f23fa383807277a93e7eb4cf869'
-    version '1.4.2'
+    sha256 'fb99bbf39775c5a56873b181125d1b2127ba2c24eb1c212c0071c76abf831a03'
+    version '1.5.1'
 
     def install
         bin.install "doxypress"
