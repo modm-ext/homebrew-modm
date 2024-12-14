ff17b87a9eae2bc05571b577df82ab518f9edda9
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2024-12-14 23:59:58 +0100

    Update Doxypress to v1.7.0

--- Formula/doxypress.rb
+++ Formula/doxypress.rb
@@ -4,10 +4,10 @@ class Doxypress < Formula
     desc 'DoxyPress: C++ documentation generator'
     homepage 'https://www.copperspice.com/documentation-doxypress.html'
 
-    url 'https://github.com/copperspice/doxypress/releases/download/dp-1.5.1/doxypress-1.5.1-osx11.5-x64.tar.bz2'
+    url 'https://github.com/copperspice/doxypress/releases/download/dp-1.7.0/doxypress-1.7.0-osx11.5-x64.tar.bz2'
 
-    sha256 'fb99bbf39775c5a56873b181125d1b2127ba2c24eb1c212c0071c76abf831a03'
-    version '1.5.1'
+    sha256 'e0c36769c471cfa50116ebac091d997cd069c43a35755537a5b60da391e2fa3e'
+    version '1.7.0'
 
     def install
         bin.install "doxypress"
